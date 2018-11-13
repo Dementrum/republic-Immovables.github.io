166098f182d489ceeb6fbe36f504e1a50cdf3b9d
Dementrum <dmi56756877@yandex.ru>
2018-11-14 02:50:25 +0900

    map2

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -4,12 +4,15 @@ $(document).ready(function () {
 
 
 function initMap() {
+	
 	var centerLatLng = new google.maps.LatLng(61.782793, 34.383885);
 	var mapOptions = {
 		center: centerLatLng,
-		zoom: 16.7
+		zoom: 16.7,
+		disableDefaultUI: true,
 	};
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	
 	var marker = new google.maps.Marker({
 		position: centerLatLng,
 		map: map,
@@ -20,6 +23,8 @@ function initMap() {
 	});
 
 }
+
 google.maps.event.addDomListener(window, "load", initMap);
+
 
 
