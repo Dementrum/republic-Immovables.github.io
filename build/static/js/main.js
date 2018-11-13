16b7d8bf4e5264e6a90b3baf99dd21cac484c328
Kolesnikov Dmitry - Slash <dmi56756877@mail.ru>
2018-11-13 17:53:23 +0900

    v 1.08 make map

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -22,11 +22,11 @@ $(document).ready(function () {
 // initMap() - функция инициализации карты
 function initMap() {
 	// Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
-	var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
+	var centerLatLng = new google.maps.LatLng(61.782793, 34.383885);
 	// Обязательные опции с которыми будет проинициализированна карта
 	var mapOptions = {
 		center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
-		zoom: 8               // Зум по умолчанию. Возможные значения от 0 до 21
+		zoom: 16.7              // Зум по умолчанию. Возможные значения от 0 до 21
 	};
 	// Создаем карту внутри элемента #map
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
