1323dd7770471623d3348e772c288f57a92c0832
Kolesnikov Dmitry - Slash <dmi56756877@mail.ru>
2018-11-16 16:02:32 +0900

    merge

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -23,14 +23,19 @@ $(document).ready(function () {
 function initMap() {
 	// Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
 	var centerLatLng = new google.maps.LatLng(61.782793, 34.383885);
+	var uluru = (61.782793, 34.383871);
+	var marker = new google.maps.Marker({position: uluru, map: map})
 	// Обязательные опции с которыми будет проинициализированна карта
 	var mapOptions = {
 		center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
 		zoom: 16.7              // Зум по умолчанию. Возможные значения от 0 до 21
+	
 	};
+	
 	// Создаем карту внутри элемента #map
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+
 // Ждем полной загрузки страницы, после этого запускаем initMap()
 google.maps.event.addDomListener(window, "load", initMap);
 
